414eca064247efe82928690e541acfc26b643d0e
lqy <3036259732@qq.com>
2022-05-31 11:39:05 +0800

    test version:1.0

--- game/views.py
+++ game/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    line1 ='<h1 style ="text-align:center">my first web</h1>'
+    return HttpResponse(line1)
 
 # Create your views here.
