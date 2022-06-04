e1670bfe1133b8ca14c0eef285affdc969419d32
lqy <3036259732@qq.com>
2022-06-04 08:53:08 +0800

    Organize file structure

--- game/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    line1 ='<h1 style ="text-align:center">my first web</h1>'
-    return HttpResponse(line1)
-
-# Create your views here.
