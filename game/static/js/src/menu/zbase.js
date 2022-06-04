f2759072d3041202f30a4cf8e5571594c06364e1
lqy <3036259732@qq.com>
2022-06-04 11:21:20 +0800

    set up background for menu

--- game/static/js/src/menu/zbase.js
+++ game/static/js/src/menu/zbase.js
@@ -1,5 +1,25 @@
-class AcGame{
-    constructor(id){
-            
+class AcGameMenu{
+    constructor(root){
+        this.root = root;
+        this.$menu = $(`
+<div class="ac_game_menu">
+    <div class="ac_game_menu_field">
+        <div class="ac_game_menu_field_item ac_game_menu_field_item_single">
+            单人模式
+        </div>
+        <div class="ac_game_menu_field_item ac_game_menu_field_item_multi">
+            多人模式
+        </div>
+        <div class="ac_game_menu_field_item ac_game_menu_field_item_settings">
+            设置
+        </div>
+    </div>
+</div>
+`);
+        this.root.$ac_game.append(this.$menu);
+        this.$single = this.$menu.find('.ac_game_menu_field_item_single');
+        this.$multi = this.$menu.find('.ac_game_menu_field_item_multi');
+        this.$settings = this.$menu.find('.ac_game_menu_field_item_settings');
+
     }
 }
