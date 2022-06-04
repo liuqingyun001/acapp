e4c375be213617c85337834873952a10c3d9cd00
lqy <3036259732@qq.com>
2022-06-04 15:53:11 +0800

    write menu  interface

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -4,12 +4,14 @@ class AcGameMenu{
         this.$menu = $(`
 <div class="ac_game_menu">
     <div class="ac_game_menu_field">
-        <div class="ac_game_menu_field_item ac_game_menu_field_item_single">
+        <div class="ac_game_menu_field_item ac_game_menu_field_item_single_mode">
             单人模式
         </div>
-        <div class="ac_game_menu_field_item ac_game_menu_field_item_multi">
+        <br>
+        <div class="ac_game_menu_field_item ac_game_menu_field_item_multi_mode">
             多人模式
         </div>
+        <br>
         <div class="ac_game_menu_field_item ac_game_menu_field_item_settings">
             设置
         </div>
@@ -17,10 +19,59 @@ class AcGameMenu{
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
-        this.$single = this.$menu.find('.ac_game_menu_field_item_single');
-        this.$multi = this.$menu.find('.ac_game_menu_field_item_multi');
+        this.$single_mode = this.$menu.find('.ac_game_menu_field_item_single_mode');
+        this.$multi_mode = this.$menu.find('.ac_game_menu_field_item_multi_mode');
         this.$settings = this.$menu.find('.ac_game_menu_field_item_settings');
 
+        this.start();
+    }
+    start(){
+        this.add_listening_events();
+    }
+
+    add_listening_events(){
+        let outer = this;
+        this.$single_mode.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+        this.$multi_mode.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+        this.$settings.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+    }
+
+    show(){
+        this.$menu.show();
+    }
+
+    hide(){
+        this.$menu.hide();
+    }
+}
+class AcGamePlayground{
+    constructor(root){
+        this.root = root;
+        this.$playground =$(`<div>play </div>`);
+
+        this.hide();
+        this.root.$ac_game.append(this.$playground);
+
+        this.start();
+    }
+    start(){
+    }
+
+    show(){
+        this.$playground.show();
+    }
+
+    hide(){
+        this.$playground.hide();
     }
 }
 class AcGame{
@@ -28,5 +79,11 @@ class AcGame{
         this.id = id;
         this.$ac_game = $('#' + id);
         this.menu = new AcGameMenu(this);
+        this.playground =new AcGamePlayground(this);
+
+        this.start();
+    }
+
+    start(){
     }
 }
